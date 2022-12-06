436ee4cca83d24ab08ed71df0fbb5e589629cfeb
rodeknopje <mn.stokhorst@gmail.com>
2022-12-06 17:12:48 +0100

    day 6

---
+++ AOC/AOC.Solutions/D06.cs
@@ -0,0 +1,26 @@
+﻿namespace AOC.Solutions;
+
+public class D06 : DayBase
+{
+    protected override int Day => 6;
+
+    public override long Solve_1() => GetFirstUniqueString(4);
+    public override long Solve_2() => GetFirstUniqueString(14);
+    
+    private int GetFirstUniqueString(int length)
+    {
+        var input = GetInputRaw();
+        
+        for (var i = length; i < input.Length; i++)
+        {
+            var part = input.Substring(i - length,length);
+            
+            if (part.Distinct().Count() == length)
+            {
+                return i;
+            }
+        }
+
+        return 0;
+    }
+}
